237b2f10a469d39b9a9254cdbb78ab134819b7bf
lzy0471 <163015764+lzy0471@users.noreply.github.com>
2024-03-11 23:20:47 +0800

    Add files via upload
    用微信开发者工具做好的小程序界面

---
+++ weixin/pages/vision/vision.js
@@ -0,0 +1,74 @@
+// pages/vision/vision.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    grids:[
+      {name:'人脸检测',image:'/static/images/g1-1.png'},
+      {name:'人脸勾勒',image:'/static/images/g1-2.png'},
+      {name:'人脸截取',image:'/static/images/g1-3.png'},
+      {name:'人脸化妆',image:'/static/images/g1-4.png'},
+      {name:'人脸特征',image:'/static/images/g1-5.png'},
+      {name:'关于作者',image:'/static/images/g1-6.png'}
+    ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
