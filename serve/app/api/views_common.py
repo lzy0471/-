df5ea92597bc6885d250a85057331049f459710a
lzy0471 <163015764+lzy0471@users.noreply.github.com>
2024-03-12 15:06:37 +0800

    Add files via upload
    添加用户代理user agent的访问跟踪代码

--- serve/app/api/views_common.py
+++ serve/app/api/views_common.py
@@ -2,16 +2,20 @@
 import tornado.web
 from concurrent.futures import ThreadPoolExecutor
 from app.common.ip2Addr import ip2addr
+
+
 class CommonHandler(tornado.web.RequestHandler):
-    executor = ThreadPoolExecutor(1000)  #定义线程池
+    executor = ThreadPoolExecutor(1000)  # 定义线程池
+
     @property
     def md(self):
         return self.application.md
 
-#公共参数
+    # 公共参数
     def common_params(self):
         data = dict(
             ip=self.request.remote_ip,
-            addr=ip2addr(self.request.remote_ip)['region'].decode('utf-8')
+            addr=ip2addr(self.request.remote_ip)['region'].decode('utf-8'),
+            headers=dict(self.request.headers)
         )
         return data
