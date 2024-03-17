bc366a6a705676986360c7e3caf03601d07d2a78
lzy0471 <163015764+lzy0471@users.noreply.github.com>
2024-03-17 12:00:24 +0800

    Add files via upload
    封装了绘制68个特征点的功能

--- serve/app/common/face.py
+++ serve/app/common/face.py
@@ -1,9 +1,10 @@
 # -*- coding:utf-8 -*-
-import os
 import datetime
+import os
 import uuid
-from unittest import result
 
+import cv2
+import dlib
 import face_recognition
 from PIL import Image, ImageDraw
 
@@ -44,28 +45,26 @@ class Face:
     def dt(self):
         return datetime.datetime.now().strftime("%Y%m%d%H%M")
 
-
-    #保存名称定义
+    # 保存名称定义
     @property
     def image_name(self):
         prefix1 = self.dt
         prefix2 = uuid.uuid4().hex
         return prefix1 + prefix2
 
-    #保存识别结果
-    def save_pil_face(self,pil_image):
-        if not os.path.exists(self.save_path):  #如果路径不存在
+    # 保存识别结果
+    def save_pil_face(self, pil_image):
+        if not os.path.exists(self.save_path):  # 如果路径不存在
             os.makedirs(self.save_path)
-        #定义图片完整名称
+        # 定义图片完整名称
         filename = "{}.png".format(
             self.image_name
         )
-        #保存图片至指定位置
-        pil_image.save(os.path.join(self.save_path,filename),'png')
+        # 保存图片至指定位置
+        pil_image.save(os.path.join(self.save_path, filename), 'png')
         return filename
 
-
-     #1.框选人脸的功能
+    # 1.框选人脸的功能
     def face_box(self):
         # 获取人脸的位置
         image, face_locations = self.get_face_locations()
@@ -80,21 +79,21 @@ class Face:
                     (left, top),
                     (right, bottom)
                 ),
-                outline=(0,255,0),
+                outline=(0, 255, 0),
                 width=5
             )
 
-        #pil_image.show()
-        #保存图像
+        # pil_image.show()
+        # 保存图像
         filename = self.save_pil_face(pil_image)
         return [filename]
 
-    #2.人脸勾勒
+    # 2.人脸勾勒
     def face_sence(self):
-        #获取图像和人脸特征
-        fece_landmarks_list,image = self.get_face_landmarks()
-        #定义获取脸部特征列表
-        facial_features =[
+        # 获取图像和人脸特征
+        fece_landmarks_list, image = self.get_face_landmarks()
+        # 定义获取脸部特征列表
+        facial_features = [
             "chin",
             "left_eyebrow",
             "right_eyebrow",
@@ -105,66 +104,111 @@ class Face:
             "bottom_lip"
 
         ]
-        #将图像转化为数组
+        # 将图像转化为数组
         pil_image = Image.fromarray(image)
-        #将图像转化为可绘制对象
+        # 将图像转化为可绘制对象
         draw = ImageDraw.Draw(pil_image)
-        #遍历所有特征点
+        # 遍历所有特征点
         for face_landmark in fece_landmarks_list:
-            #遍历特征列表
+            # 遍历特征列表
             for facial_feature in facial_features:
-                #绘制描线
-                draw.line(face_landmark[facial_feature],width=5,fill=(255,255,255))
-        #显示图像
+                # 绘制描线
+                draw.line(face_landmark[facial_feature], width=5, fill=(255, 255, 255))
+        # 显示图像
 
-        #pil_image.show()
-        #保存图像
+        # pil_image.show()
+        # 保存图像
         filename = self.save_pil_face(pil_image)
         return [filename]
+
     # 3.人脸截取
     def face_find(self):
-        #获取图像和人脸位置
-        image,face_locations = self.get_face_locations()
+        # 获取图像和人脸位置
+        image, face_locations = self.get_face_locations()
         result = []
         for face_location in face_locations:
             top, right, bottom, left = face_location
-            #框选图片中所有人
+            # 框选图片中所有人
             face_image = image[top:bottom, left:right]
-            #框选结果化数组
+            # 框选结果化数组
             pil_image = Image.fromarray(face_image)
             filename = self.save_pil_face(pil_image)
             result.append(
                 filename
             )
         return result
+
     # 4.人脸化妆
     def face_makeup(self):
-        #获取图像和人脸位置
-        face_landmarks_list,image = self.get_face_landmarks()
-        #将图像转化为图像数组
+        # 获取图像和人脸位置
+        face_landmarks_list, image = self.get_face_landmarks()
+        # 将图像转化为图像数组
         pil_image = Image.fromarray(image)
-        #将图像数组转化为可绘制的对象
-        draw = ImageDraw.Draw(pil_image,'RGBA')  #A是透明度
-        #循环遍历人脸
+        # 将图像数组转化为可绘制的对象
+        draw = ImageDraw.Draw(pil_image, 'RGBA')  # A是透明度
+        # 循环遍历人脸
         for face_landmark in face_landmarks_list:
-            #绘制左右眉毛
-            draw.polygon(face_landmark['left_eyebrow'],fill=(68,0,0,128))
+            # 绘制左右眉毛
+            draw.polygon(face_landmark['left_eyebrow'], fill=(68, 0, 0, 128))
             draw.polygon(face_landmark['right_eyebrow'], fill=(68, 0, 0, 128))
-            draw.line(face_landmark['left_eyebrow'], fill=(68, 0, 0, 150),width=3)
-            draw.line(face_landmark['right_eyebrow'],fill=(68,0,0,150),width=3)
-            #绘制上下嘴唇
+            draw.line(face_landmark['left_eyebrow'], fill=(68, 0, 0, 150), width=3)
+            draw.line(face_landmark['right_eyebrow'], fill=(68, 0, 0, 150), width=3)
+            # 绘制上下嘴唇
             draw.polygon(face_landmark['top_lip'], fill=(150, 0, 0, 128))
             draw.polygon(face_landmark['bottom_lip'], fill=(150, 0, 0, 128))
-            #绘制左右眼睛
-            draw.polygon(face_landmark['left_eye'], fill=(255,255,255,30))
-            draw.polygon(face_landmark['right_eye'], fill=(255,255,255,30))
-            draw.line(face_landmark['left_eye']+[face_landmark['right_eye'][0]], fill=(0,0,0,110),width=2)
-            draw.line(face_landmark['right_eye']+[face_landmark['right_eye'][0]], fill=(0, 0, 0, 110), width=2)
-        #保存图片
+            # 绘制左右眼睛
+            draw.polygon(face_landmark['left_eye'], fill=(255, 255, 255, 30))
+            draw.polygon(face_landmark['right_eye'], fill=(255, 255, 255, 30))
+            draw.line(face_landmark['left_eye'] + [face_landmark['right_eye'][0]], fill=(0, 0, 0, 110), width=2)
+            draw.line(face_landmark['right_eye'] + [face_landmark['right_eye'][0]], fill=(0, 0, 0, 110), width=2)
+        # 保存图片
         filename = self.save_pil_face(pil_image)
         return [filename]
-        #pil_image.show()
-    # 人脸
+        # pil_image.show()
+
+    # 5.人脸68个特征点获取
+    def face_68_point(self):
+        image = cv2.imread(os.path.join(self.dir_path, self.pic_name))
+        # 使用特征提取器
+        detector = dlib.get_frontal_face_detector()
+        model_path = os.path.join(
+            os.path.dirname
+            (os.path.dirname(__file__)
+             ),
+            'static/models/shape_predictor_68_face_landmarks.dat'
+        )
+        # 定义预测器
+        predictor = dlib.shape_predictor(model_path)
+        dets = detector(image, 1)
+        for k, d in enumerate(dets):
+            # 利用预测器预测
+            shape = predictor(image, d)
+            # 标出68个点
+            for i in range(68):
+                cv2.circle(image,
+                           (shape.part(i).x, shape.part(i).y), 3, (255, 0, 0),-1  # 这里是BGR
+                           )  # 获取圆心
+                cv2.putText(image,
+                            str(i),
+                            (shape.part(i).x,shape.part(i).y), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0),
+                            )
+        # cv2.imshow('68.png',image)
+        # cv2.waitKey(0)
+        #保存图片到指定位置
+        filename = self.save_cv2_face(image)
+        return [filename]
+
+    # 使用opencv保存图片
+    def save_cv2_face(self, image):
+        if not os.path.exists(self.save_path):  # 如果路径不存在
+            os.makedirs(self.save_path)
+        # 定义图片完整名称
+        filename = "{}.png".format(
+            self.image_name
+        )
+        # 保存图片至指定位置
+        cv2.imwrite(os.path.join(self.save_path, filename), image)
+        return filename
 
 
 if __name__ == "__main__":
@@ -177,5 +221,5 @@ if __name__ == "__main__":
         save_path=os.path.join(root_path, 'static/uploads')
     )
     # print(face.get_face_locations())
-    #print(face.get_face_landmarks())
-    print(face.face_makeup())
+    # print(face.get_face_landmarks())
+    print(face.face_68_point())
