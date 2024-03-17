6ec6c4a7a5b70dedc8285822b1b60cafed473f76
lzy0471 <163015764+lzy0471@users.noreply.github.com>
2024-03-17 23:59:19 +0800

    Add files via upload
    进行了人脸识别结果与接口的对接工作，实现了上传图片后自动跳转到识别结果页面

---
+++ weixin1/pages/vision/vision.js
@@ -0,0 +1,33 @@
+// pages/vision/vision.js
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    grids:null
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    var self = this;
+    wx.request({
+      url:app.globalData.url+ '/grid/',
+      header:{
+        'content-type':'application/json'
+      },
+      method:'GET',
+      success:function(res){
+        console.log(res);
+        self.setData({
+          grids: res.data
+        })
+      }
+    })
+
+  },
+})
+  
